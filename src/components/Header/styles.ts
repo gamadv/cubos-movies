efc00ee5d42116a3dad73c2be4c93ed881896bca
Gamadv <lealdev.py@gmail.com>
2021-04-01 23:26:22 -0300

    ft: Create Header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 70px;
+  background-color: #116193;
+
+  h1 {
+    font-weight: 500;
+    color: #06d9dc;
+  }
+`;
